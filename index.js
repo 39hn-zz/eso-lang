4c12314a1eaf87d726361597b93a234b335e8d60
Sam Goo <admin@the.gg>
2018-04-17 09:40:21 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const parser = require('./lib/langparser');
+const compiler = require('./lib/langcompiler');
+
+module.exports = {
+  compiler,
+  parser
+};
---
+++ test.js
@@ -0,0 +1,26 @@
+const esolang = require('./index');
+const fs = require('fs');
+
+const { parser, compiler } = esolang;
+
+const testRecords = [{ Id: 3427285, Unknown: 0, Index: 3, Offset: 0, Text: 'TEST1' },
+  { Id: 3427285, Unknown: 0, Index: 7, Offset: 5, Text: 'TEST2' },
+  { Id: 3427285, Unknown: 0, Index: 8, Offset: 0, Text: '테스트3' },
+  { Id: 3427285, Unknown: 0, Index: 9, Offset: 11, Text: '테스트4' }];
+
+console.log('Now Compiling following records...');
+console.log(testRecords);
+const buf = compiler.compile(testRecords);
+console.log('Done!');
+
+console.log('Now Writing compiled records as "test.lang"...');
+fs.writeFileSync('./test.lang', buf);
+console.log('Done!')
+
+console.log('Now Reading records from "test.lang"...');
+const buffer = fs.readFileSync('./test.lang');
+console.log('Done!');
+
+console.log('The results are...');
+const records = parser.parse(buffer);
+console.log(records);
